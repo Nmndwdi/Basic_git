c5691047b6532631d6394d1ec25ea73b3711aee5
naman <Nmndwdi1001@gmail.com>
2023-07-17 14:48:27 +0530

    all files commmited

--- B.java
+++ B.java
@@ -1,5 +1,7 @@
 public class B {
     public static void main(String[] args) {
         System.out.println("I am in B class");
+        System.out.println("I am in B class : second print statement");
+
     }
 }
